bd22eb10d862ede61aece869e94cc5b2507959d2
Mohammed <moohyacc@gmail.com>
2019-12-22 01:21:42 +0300

    fixing db tables

--- db/migrate/20191217055937_create_books.rb
+++ db/migrate/20191217055937_create_books.rb
@@ -1,5 +1,13 @@
 class CreateBooks < ActiveRecord::Migration[6.0]
   def change
+    create_table :authors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :genre
+      t.date :birthdate
+
+      t.timestamps
+    end
     create_table :books do |t|
       t.references :author, null: false, foreign_key: true
       t.string :name
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_17_055937) do
+ActiveRecord::Schema.define(version: 2019_12_21_221923) do
 
-  create_table "authors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "authors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "genre"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_12_17_055937) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "author_id", null: false
     t.string "name"
     t.string "genre"
